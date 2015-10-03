53f98bce8e91ae4ceebc4a0bd0dcedba6e3f9ce1
CatCfrco <z82206.cat@gmail.com>
2015-10-03 10:59:21 +0800

    Init

---
+++ app/src/main/java/tw/meowdev/android/starter/app/StarterApplication.java
@@ -0,0 +1,68 @@
+package tw.meowdev.android.starter.app;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+public class StarterApplication extends Application {
+    private static final String TAG = "application";
+    public void onCreate() {
+        super.onCreate();
+
+        registerActivityLifecycleCallbacks(activityLifeCycleCallbacks);
+    }
+
+    static class State_ {
+        private Activity currentActivity;
+        public Activity getCurrentActivity() {
+            return currentActivity;
+        }
+    }
+
+    private final static State_ State = new State_();
+
+    private final static Application.ActivityLifecycleCallbacks activityLifeCycleCallbacks = new Application.ActivityLifecycleCallbacks() {
+
+        private void logActivityEvent(Activity activity, String event) {
+            Log.d(TAG, "Activity:" + activity.getLocalClassName() + " " + event);
+        }
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            logActivityEvent(activity, "created");
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+            logActivityEvent(activity, "started");
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            logActivityEvent(activity, "resumed");
+            State.currentActivity = activity;
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+            logActivityEvent(activity, "paused");
+            State.currentActivity = null;
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+            logActivityEvent(activity, "stopped");
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            logActivityEvent(activity, "saveInstanceState");
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            logActivityEvent(activity, "destroyed");
+        }
+    };
+}
